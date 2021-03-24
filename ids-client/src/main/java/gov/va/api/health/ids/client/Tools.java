10624ce1d22058880e5ab8a921a1a2f79558dea3
Bryan Schofield <43883561+bschofield-va@users.noreply.github.com>
2021-03-24 10:41:22 -0400

    `encoded-ids` tool support for switching between I2 and I3 ids. (#26)
    Mapping jar is no longer required. If not specified, no component expansion will be performed.
    
    By default both 2 and 3 are enabled, but can refined with `--i2` and `--i3` switches.

--- ids-client/src/main/java/gov/va/api/health/ids/client/Tools.java
+++ ids-client/src/main/java/gov/va/api/health/ids/client/Tools.java
@@ -1,15 +1,23 @@
 package gov.va.api.health.ids.client;
 
-import static gov.va.api.health.ids.client.EncodedIdFormat.V2_PREFIX;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import gov.va.api.health.ids.api.IdentityService;
 import gov.va.api.health.ids.api.ResourceIdentity;
 import gov.va.api.health.ids.client.EncryptingIdEncoder.Codebook;
 import gov.va.api.health.ids.client.EncryptingIdEncoder.CodebookSupplier;
+import gov.va.api.health.ids.client.IdsClientProperties.EncodedIdsFormatProperties;
+import gov.va.api.health.ids.client.IdsClientProperties.PatientIcnFormatProperties;
+import gov.va.api.health.ids.client.IdsClientProperties.UuidFormatProperties;
 import java.util.List;
 import java.util.Optional;
 import java.util.ServiceLoader;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.BooleanUtils;
+import org.slf4j.LoggerFactory;
 
 @NoArgsConstructor(staticName = "tools")
 public class Tools {
@@ -19,6 +27,13 @@ public class Tools {
 
   /** Tool main called from command line. */
   public static void main(String[] args) {
+    try {
+      LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+      Logger rootLogger = lc.getLogger(Logger.ROOT_LOGGER_NAME);
+      rootLogger.setLevel(Level.OFF);
+    } catch (Exception e) {
+      // ignored... we tried to disabled logging.
+    }
     if (args.length == 1) {
       tools().decode(args[0]);
     } else if (args.length == 3) {
@@ -41,28 +56,51 @@ public class Tools {
   }
 
   private void decode(String id) {
-    ResourceIdentity identity = encoder().decode(id.replace(V2_PREFIX, ""));
+    List<ResourceIdentity> identities = encoder().lookup(id);
+    if (identities.isEmpty()) {
+      throw new IllegalArgumentException("Cannot determine identity of " + id);
+    }
+    ResourceIdentity identity = identities.get(0);
     System.out.println(identity.system() + " " + identity.resource() + " " + identity.identifier());
+  }
+
+  private boolean enabled(String property) {
+    return BooleanUtils.toBoolean(System.getProperty(property, "true"));
   }
 
   private void encode(String system, String resource, String identifier) {
     System.out.println(
-        V2_PREFIX
-            + encoder()
-                .encode(
+        encoder()
+            .register(
+                List.of(
                     ResourceIdentity.builder()
                         .system(system)
                         .resource(resource)
                         .identifier(identifier)
-                        .build()));
+                        .build()))
+            .get(0)
+            .uuid());
   }
 
-  private EncryptingIdEncoder encoder() {
+  private IdentityService encoder() {
+    var properties =
+        IdsClientProperties.builder()
+            .patientIcn(
+                PatientIcnFormatProperties.builder()
+                    .enabled(enabled("patient"))
+                    .idPattern("[0-9]+(V[0-9]{6})?")
+                    .build())
+            .encodedIds(
+                EncodedIdsFormatProperties.builder()
+                    .i2Enabled(enabled("i2"))
+                    .i3Enabled(enabled("i3"))
+                    .encodingKey(password())
+                    .build())
+            .uuid(UuidFormatProperties.builder().enabled(false).build())
+            .build();
+    var config = new RestIdentityServiceClientConfig(null, properties, null);
     Optional<CodebookSupplier> codebooks = ServiceLoader.load(CodebookSupplier.class).findFirst();
-    return EncryptingIdEncoder.builder()
-        .password(password())
-        .codebook(codebooks.orElseGet(EmptyCodebookSupplier::new).get())
-        .build();
+    return config.encodingIdentityServiceClient(codebooks.orElse(Codebook::empty).get());
   }
 
   private String password() {
@@ -75,13 +113,6 @@ public class Tools {
       throw new MissingProperty(name);
     }
     return value;
-  }
-
-  private static class EmptyCodebookSupplier implements CodebookSupplier {
-    @Override
-    public Codebook get() {
-      return Codebook.builder().build();
-    }
   }
 
   static final class MissingProperty extends RuntimeException {
